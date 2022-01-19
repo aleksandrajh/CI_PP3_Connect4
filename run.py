7f7a97856c390ef540c2f97f0e9fd8d224cfde55
Aleksandra Haniok <a.haniok@gmail.com>
2022-01-19 16:37:48 +0000

    Create a welcome page

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def welcome():
+    """
+    Add welcome page
+    Display game name and author
+    """
+print(" ")
+print(" ")
+print(" _____                                   _        ___ ")
+print("/  __ \                                 | |      /   |")
+print("| /  \/  ___   _ __   _ __    ___   ___ | |_    / /| |")
+print("| |     / _ \ |  _ \ |  _ \  / _ \ / __|| __|  / /_| |")
+print("| \__/\| (_) || | | || | | ||  __/| (__ | |_   \___  |")
+print(" \____/ \___/ |_| |_||_| |_| \___| \___| \__|      |_/")
+print(" ")
+print("           Created by Aleksandra H.")
+print(" ")
+print(" ")
+
+
+def main():
+    """
+    Run all program functions
+    """
+    welcome()
+
+if __name__ == "__main__":
+    main()
