c4c6783a35b086acb0ff4dcbd6d004f07072e66e
Aleksandra Haniok <a.haniok@gmail.com>
2022-01-20 13:18:57 +0000

    Amend function to ask for correct game option

--- run.py
+++ run.py
@@ -25,23 +25,41 @@ print(" ")
 
 def select_game():
     """
-    the program will first show to possible options
-    user can select a game for 2 or 1 player
+    the program will first show two possible options
+    user can select a game for either 2 or 1 player
     """
-    print("Select game option:\n")
-    game_options = "1) 2 Players 2) Player vs. Computer\n"
+    print("Select game option:")
+    game_options = "1) 2 Players \n2) Player vs. Computer\n"
     game_selected = input(game_options)
-           
+    separate_line()
+
     if game_selected == "1":
         print("2 players game selected")
+        print(" ")
+        get_players_names()
 
     elif game_selected == "2":
         print("Player vs computer")
+        print(" ")
+        # Add function for a game with computer
 
-    else:
-        print("Not a valid option, please try again")
-        print("Select game option:\n")
+    # Validate if answers is either 1 or 2    
+    while game_selected not in ("1", "2"):
+        print("Please choose between one of the options.")
         game_selected = input(game_options)
+
+        separate_line()
+
+    return game_selected
+
+
+def separate_line():
+    """
+    Print a - line to end the sections
+    """
+    print(" ")
+    print("- "*20)
+    print(" ")
 
 
 
