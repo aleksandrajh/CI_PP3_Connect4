100133fe2cf697b71cd8df8ba722d14d8b3ab7b5
Aleksandra Haniok <a.haniok@gmail.com>
2022-02-14 11:43:08 +0000

    Add doc strings to test_validation file

--- test_validation.py
+++ test_validation.py
@@ -3,6 +3,10 @@ import validation as val
 
 
 class TestValidate(unittest.TestCase):
+    """
+    Verification of the user email and user name 
+    input values and types
+    """
     def test_validate_email(self):
         self.assertTrue(val.validate_user_email('testemail@gmail.com'), True)
 
@@ -19,6 +23,10 @@ class TestValidate(unittest.TestCase):
 
 
 class TestUsersLogInOrRegistration(unittest.TestCase):
+    """
+    Verification of the input parameters
+    for the players log in and registration
+    """
     def test_log_in(self):
         self.assertTrue(val.log_in_players(['User1', 'User2']), True)
         self.assertEqual(val.log_in_players(123), None)
